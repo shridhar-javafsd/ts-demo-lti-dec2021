8d9d986a1a07b708d9115c63a928e86f5703331f
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-20 09:39:35 +0530

    C

--- 02-ts-class.js
+++ 02-ts-class.js
@@ -1,12 +1,25 @@
+// proper TS code 
 var Book = /** @class */ (function () {
     function Book() {
     }
     Book.prototype.publish = function () {
-        this.title = "Some Book Name";
-        this.author = "Author Name";
-        console.log(this.title + this.author);
+        this.title = "harry Potter";
+        this.author = "Jeckaline";
+        console.log(this.title + " " + this.author);
     };
     return Book;
 }());
 var bookObj = new Book();
 bookObj.publish();
+// vanilla JS code 
+// class Book {
+//     title;
+//     author;
+//     publish() {
+//         this.title = 200;
+//         this.author = 225;
+//         console.log(this.title + this.author);
+//     }
+// }
+// const bookObj = new Book();
+// bookObj.publish();
--- 02-ts-class.ts
+++ 02-ts-class.ts
@@ -1,17 +1,35 @@
+// proper TS code 
 
 class Book {
-    title;
-    author;
+    title: string;
+    author: string;
 
     publish() {
-        this.title = "Some Book Name";
-        this.author = "Author Name";
-        console.log(this.title + this.author);
+        this.title = "Harry Potter";
+        this.author = "Jeckaline";
+        console.log(this.title + " " + this.author);
     }
 }
-
 
 const bookObj = new Book();
 
 bookObj.publish();
+
+
+// vanilla JS code 
+
+// class Book {
+//     title;
+//     author;
+
+//     publish() {
+//         this.title = 200;
+//         this.author = 225;
+//         console.log(this.title + this.author);
+//     }
+// }
+
+// const bookObj = new Book();
+
+// bookObj.publish();
 
