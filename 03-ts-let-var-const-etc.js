ca7e4328842e00323a2e30f1fa458e1cc474ebfc
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-20 09:47:56 +0530

    C

---
+++ 03-ts-let-var-const-etc.js
@@ -0,0 +1,15 @@
+// tsc 03-ts-let-var-const-etc.ts
+// node 03-ts-let-var-const-etc.js
+// JS code 
+// num1 = 10;
+// var num2 = 20;
+var num3 = 30;
+var num4 = 40;
+// console.log(num1);
+// console.log(num2);
+console.log(num3);
+console.log(num4);
+num3 = 35;
+num4 = 45;
+console.log(num3);
+console.log(num4);
---
+++ 03-ts-let-var-const-etc.ts
@@ -0,0 +1,19 @@
+// tsc 03-ts-let-var-const-etc.ts
+// node 03-ts-let-var-const-etc.js
+
+// JS code 
+
+// num1 = 10;
+// var num2 = 20;
+let num3 = 30;
+const num4 = 40;
+
+// console.log(num1);
+// console.log(num2);
+console.log(num3);
+console.log(num4);
+
+num3 = 35;
+// num4 = 45; // error 
+console.log(num3);
+console.log(num4);
