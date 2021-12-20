3883ef186e1b58215ee5aaf293e2a41065486e0c
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-20 10:24:30 +0530

    C

---
+++ 05-ts-type-notation.js
@@ -0,0 +1,8 @@
+// tsc 05-ts-type-notation.ts
+// node 05-ts-type-notation.js
+var num = 10;
+var num2 = 20;
+console.log(num);
+console.log(num2);
+num2 = false;
+console.log(num2);
---
+++ 05-ts-type-notation.ts
@@ -0,0 +1,12 @@
+// tsc 05-ts-type-notation.ts
+// node 05-ts-type-notation.js
+
+let num = 10;
+let num2: number = 20;
+
+console.log(num);
+console.log(num2);
+
+num2 = false;
+console.log(num2);
+
